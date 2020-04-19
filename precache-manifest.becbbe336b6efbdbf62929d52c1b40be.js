004d70b8f9e9ce89e497234613a406f5557fbe99
maeve <mujammilkhan321@gmail.com>
2020-04-20 02:35:15 +0530

    Updates

---
+++ precache-manifest.becbbe336b6efbdbf62929d52c1b40be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d98dc8cac2410d12c6458ed9772aa7d",
+    "url": "/EpubReader/index.html"
+  },
+  {
+    "revision": "52b34b75322fc8f205f1",
+    "url": "/EpubReader/static/js/2.8a431489.chunk.js"
+  },
+  {
+    "revision": "872d4ad223c39fec1068d862c0b329ff",
+    "url": "/EpubReader/static/js/2.8a431489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7629d1bbbf82001b554c",
+    "url": "/EpubReader/static/js/main.13ee06e6.chunk.js"
+  },
+  {
+    "revision": "3ddba381c1444869c11c",
+    "url": "/EpubReader/static/js/runtime-main.db9415e4.js"
+  },
+  {
+    "revision": "783d189ec9060061841987bd11f5da70",
+    "url": "/EpubReader/static/media/loader.783d189e.gif"
+  }
+]);
